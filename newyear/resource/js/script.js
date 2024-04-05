8128ca24ebfa7176630767795a8f24481f4ac84a
raintien <166023580+raintien@users.noreply.github.com>
2024-04-05 14:56:14 +0800

    newyear

---
+++ newyear/resource/js/script.js
@@ -0,0 +1,20 @@
+﻿
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  const snowflakesContainer = document.querySelector(".snowflakes");
+
+  // 添加更多的雪花元素，根据需要调整数量
+  for (let i = 0; i < 15; i++) {
+    createSnowflake();
+  }
+
+  function createSnowflake() {
+    const snowflake = document.createElement("div");
+    snowflake.className = "snowflake";
+    snowflake.style.left = `${Math.random() * 100}vw`;
+    snowflake.style.animationDuration = `${Math.random() * 10+ 3}s`;
+    snowflakesContainer.appendChild(snowflake);
+
+
+  }
+});
