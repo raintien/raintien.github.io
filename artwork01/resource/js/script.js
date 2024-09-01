26d8384f4041243e40e62c835ff867c0c862128d
raintien <166023580+raintien@users.noreply.github.com>
2024-09-01 22:44:59 +0800

    Delete artwork01 directory

--- artwork01/resource/js/script.js
+++
@@ -1,9 +0,0 @@
-﻿const follower = document.querySelector(".follower");
-
-document.addEventListener("mousemove", (e) => {
-  const x = e.clientX;
-  const y = e.clientY;
-
-  follower.style.left = x + "px";
-  follower.style.top = y + "px";
-});
